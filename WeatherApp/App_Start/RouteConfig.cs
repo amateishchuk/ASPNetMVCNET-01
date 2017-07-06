4eb5d8e97e54863d06da2d6242750387bd96e766
amateishchuk <amateishchuk@gmail.com>
2017-07-06 10:02:34 +0300

    route constraints

--- WeatherApp/App_Start/RouteConfig.cs
+++ WeatherApp/App_Start/RouteConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
 using System.Web.Routing;
 
 namespace WeatherApp
@@ -12,13 +13,17 @@ namespace WeatherApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute("OnlyCity", "{city}", new { controller = "Weather", action = "ShowWeather" });
 
+            routes.MapRoute("OnlyCity", "{city}", new { controller = "Weather", action = "ShowWeather" },
+                new { city = new AlphaRouteConstraint() });
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{city}/{qtyDays}",
-                defaults: new { controller = "Weather", action = "ShowWeather" }
+                defaults: new { controller = "Weather", action = "ShowWeather" },
+                constraints: new { city = new AlphaRouteConstraint(), qtyDays = new RangeRouteConstraint(1, 17)}
             );
+
             routes.MapRoute("Empty", "", new { controller = "Weather", action = "ShowWeather" });
         }
     }
